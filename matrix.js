f2cc2f06027dbb231df3988b34ddf376abcf4f6a
© Jakob 3301™ <91836235+JakobDanielsen@users.noreply.github.com>
2022-11-14 17:19:05 +0100

    Create matrix.js

---
+++ matrix.js
@@ -0,0 +1,26 @@
+let message = "jakob er veldig kul"
+let messageIndex = 0
+
+let output = ""
+
+const ALPHABET = "abcdefghijklmnopqrstuvwxyz "
+
+
+function findLetter(){
+        for (let i = 0; i < ALPHABET.length; i++) {
+            console.log(output + ALPHABET[i]);
+            if (message.toLowerCase()[messageIndex] == ALPHABET[i]) {
+                // console.log("LETTER FOUND "+ALPHABET[i]);
+                output = output.concat(ALPHABET[i]) 
+                messageIndex++
+                break
+            }
+        }
+    if (output.length == message.length) {
+    } else {
+        // console.log(`${output} is not equal to ${message}`);
+        findLetter()
+    }
+}
+
+findLetter()
